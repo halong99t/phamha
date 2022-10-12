205c2ad979d5e71f5b48a29f189d7315f2951697
halong99t <phamhande@gmail.com>
2022-10-12 19:37:04 +0700

    remove icon loading

--- src/screens/LoadingPage.js
+++ src/screens/LoadingPage.js
@@ -2,11 +2,7 @@ import React from "react";
 import "../assets/styles.css";
 import loadingImg from "../assets/loading.svg";
 function LoadingPage() {
-  return (
-    <div className="loading">
-      <img src={loadingImg} alt="loading" />
-    </div>
-  );
+  return <div className="loading"></div>;
 }
 
 export default LoadingPage;
